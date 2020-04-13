791af4e2b40bae76f7dbca340cd8aff1272d164f
Aazmund <AazmundZ@gmail.com>
2020-04-13 03:02:04 +0300

    it's revolution, Jhony!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Main {
@@ -22,25 +20,46 @@ public class Main {
             {"9", "*", "*", "9", "9", "9", "9", "9", "*", "*", "9"},
             {"9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9"}
     };
+    private static String actionLabel = "";
+    private static String playerNameStr = ""; // "Игрок 1";
+    private static String playerGoldStr = ""; //""Золото: 500";
+    private static String playerSheepStr = ""; //""Овцы: 10";
+    private static String playerLogStr = ""; //'""Брёвна: 50";
 
+    private static JLabel playerName = (new JLabel(playerNameStr));
+    private static JLabel playerGold = (new JLabel(playerGoldStr));
+    private static JLabel playerSheep =(new JLabel(playerSheepStr));
+    private static JLabel playerLog = (new JLabel(playerLogStr));
 
     private static JButton[][] buttons = new JButton[11][11];
+    private static JButton end_button = new JButton("Начать игру");
 
+    private static JFrame mainFrame = new JFrame();
 
-   static public void main(String[] args) {
+    private static JMenuBar menuBar = new JMenuBar();
 
-        System.setProperty("apple.laf.useScreenMenuBar", "true");
-        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Name");
+    private static JMenu menu = new JMenu("Новая игра");
 
-        JFrame mainFrame = new JFrame();
+    private static JMenuItem twoPlayers = new JMenuItem("2 Игрока");
+    private static JMenuItem threePlayers = new JMenuItem("3 Игрока");
+    private static JMenuItem fourPlayers = new JMenuItem("4 Игрока");
+    private static JMenuItem fivePlayers = new JMenuItem("5 Игроков");
 
-        JMenuBar menuBar = new JMenuBar();
+    private static JPanel action = new JPanel();
+    private static JPanel playerWindow = new JPanel();
+    private static JPanel panel = new JPanel();
+    private static JPanel top = new JPanel();
+    private static JPanel bottom = new JPanel();
+    private static JPanel gameMap = new JPanel();
+    private static JPanel buttonAction = new JPanel();
 
-        JMenu menu = new JMenu("Новая игра");
-        JMenuItem twoPlayers = new JMenuItem("2 Игрока");
-        JMenuItem threePlayers = new JMenuItem("3 Игрока");
-        JMenuItem fourPlayers = new JMenuItem("4 Игрока");
-        JMenuItem fivePlayers = new JMenuItem("5 Игроков");
+    private static ArrayList<Player> players = new ArrayList<>();
+
+    private static Integer index = 0;
+
+    public static void frameConfigurator(){
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
 
         menu.add(twoPlayers);
         menu.add(threePlayers);
@@ -50,137 +69,130 @@ public class Main {
         menuBar.add(menu);
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension dimension = toolkit.getScreenSize();
         mainFrame.setBounds(dimension.width / 2 - 400, dimension.height / 2 - 300, 800, 600);
         mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         mainFrame.setTitle("Guilds game");
         mainFrame.setIconImage(null);
-
-
         mainFrame.setJMenuBar(menuBar);
+
+        action.add(new Label("История действий:") );
+        action.add(new JLabel(actionLabel));
+
+        for (int a = 0; a < 11; a++) {
+            for (int b = 0; b < 11; b++) {
+                buttons[a][b] = new JButton();
+                buttons[a][b].setBorder(null);
+                buttons[a][b].setFocusPainted(false);
+                buttons[a][b].setContentAreaFilled(false);
+                if (!tileMap[a][b].equals("*")) {
+                    buttons[a][b].setIcon(new ImageIcon("graphics/rsz_1.png"));
+                }
+                gameMap.add(buttons[a][b]);
+            }
+        }
+
+        panel.setBorder(new EmptyBorder(5,5,5,5));
+
+        top.setLayout(new BorderLayout(5,5));
+        top.add(gameMap, BorderLayout.CENTER);
+        top.add(playerWindow, BorderLayout.EAST);
+
+        bottom.setLayout(new BorderLayout(5,5));
+        bottom.add(action, BorderLayout.CENTER);
+        bottom.add(buttonAction, BorderLayout.EAST);
+
+        panel.setLayout(new BorderLayout(5,5));
+        panel.add(top, BorderLayout.CENTER);
+        panel.add(bottom, BorderLayout.SOUTH);
+
+        gameMap.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 1, Color.GRAY));
+        gameMap.setLayout(new GridLayout(11, 11, 1, 1));
+
+        buttonAction.add(end_button);
+
+        playerWindow.setLayout(new GridLayout(4, 1, 1, 1));
+        playerWindow.add(playerName);
+        playerWindow.add(playerGold);
+        playerWindow.add(playerSheep);
+        playerWindow.add(playerLog);
+
+        mainFrame.add(panel);
+        mainFrame.revalidate();
+    }
+
+    public static void game(int index){
+        end_button.setText("Завершить ход");
+        System.out.println("Player " + index);
+        int cubeScore;
+        //TODO тут поставить метод по отрисовке окна с генерацией кубика, для записи шагов использовать cubeScore
+        cubeScore = 3;
+        players.get(index).move(cubeScore);
+        //TODO сюда вкорячить метод на проверку клетки на владельца, чтото типа checkCell(player.get(x), player.get(y))
+    }
+
+    public static void startGame(){
+        end_button.addActionListener(e -> {
+            if (index == players.size()){
+                index = 0;
+                game(index);
+            }else{
+                game(index);
+                index++;
+            }
+        });
+    }
+
+    static public void main(String[] args) {
+
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Name");
+
+        frameConfigurator();
 
         mainFrame.setVisible(true);
 
         twoPlayers.addActionListener(e -> {
             MapGenerator mapGenerator = new MapGenerator();
             mapGenerator.generateMap(tileMap);
-            ArrayList<Player> players = new ArrayList<>();
             for (int i = 0; i < 2; i++) {
                 Player player = new Player();
                 player.setStartPosition();
                 players.add(player);
             }
+            startGame();
         });
 
         threePlayers.addActionListener(e -> {
             MapGenerator mapGenerator = new MapGenerator();
             mapGenerator.generateMap(tileMap);
-            ArrayList<Player> players = new ArrayList<>();
             for (int i = 0; i < 3; i++) {
                 Player player = new Player();
                 player.setStartPosition();
                 players.add(player);
             }
+            startGame();
         });
 
         fourPlayers.addActionListener(e -> {
             MapGenerator mapGenerator = new MapGenerator();
             mapGenerator.generateMap(tileMap);
-            ArrayList<Player> players = new ArrayList<>();
             for (int i = 0; i < 4; i++) {
                 Player player = new Player();
                 player.setStartPosition();
                 players.add(player);
             }
+            startGame();
         });
 
         fivePlayers.addActionListener(e -> {
             MapGenerator mapGenerator = new MapGenerator();
             mapGenerator.generateMap(tileMap);
-            ArrayList<Player> players = new ArrayList<>();
             for (int i = 0; i < 5; i++) {
                 Player player = new Player();
                 player.setStartPosition();
                 players.add(player);
             }
+            startGame();
         });
-
-        JPanel Panel = new JPanel();
-        JPanel Top = new JPanel();
-        JPanel Bottom = new JPanel();
-        ///
-        JPanel Card = new JPanel();
-
-        Card.setLayout(new GridLayout(11, 11, 1, 1));
-
-        for (int a = 0; a < 11; a++) {
-           for (int b = 0; b < 11; b++) {
-               buttons[a][b] = new JButton();
-               buttons[a][b].setBorder(null);
-               buttons[a][b].setFocusPainted(false);
-               buttons[a][b].setContentAreaFilled(false);
-               if (tileMap[a][b]!="*") {
-                   buttons[a][b].setIcon(new ImageIcon("graphics/rsz_1.png"));
-               }
-               Card.add(buttons[a][b]);
-           }
-        }
-        ///
-        JPanel Action = new JPanel();
-        Action.add(new Label("История действий:") );
-        String actionallabel = "";
-        Action.add(new JLabel(actionallabel));
-        ///
-       JPanel PlayerWindow = new JPanel();
-
-       PlayerWindow.setLayout(new GridLayout(4, 1, 1, 1));
-       String playerName = "Игрок 1";
-       JLabel Player_Name = (new JLabel(playerName));
-       String playerGold = "Золото: 500";
-       JLabel Player_Gold = (new JLabel(playerGold));
-       String playerSheep = "Овцы: 10";
-       JLabel Player_Sheep =(new JLabel(playerSheep));
-       String playerLog = "Брёвна: 50";
-       JLabel Player_Log = (new JLabel(playerLog));
-       PlayerWindow.add(Player_Name);
-       PlayerWindow.add(Player_Gold);
-       PlayerWindow.add(Player_Sheep);
-       PlayerWindow.add(Player_Log);
-       ///
-       JPanel ButtonAction = new JPanel();
-
-       JButton End_button = new JButton("Завершить ход");
-
-       ButtonAction.add(End_button);
-
-       End_button.addActionListener(new ActionListener() {
-             @Override
-             public void actionPerformed(ActionEvent actionEvent) {
-
-            }
-        });
-        ///
-        Panel.setBorder(new EmptyBorder(5,5,5,5));
-
-        Top.setLayout(new BorderLayout(5,5));
-        Top.add(Card, BorderLayout.CENTER);
-        Top.add(PlayerWindow, BorderLayout.EAST);
-
-        Bottom.setLayout(new BorderLayout(5,5));
-        Bottom.add(Action, BorderLayout.CENTER);
-        Bottom.add(ButtonAction, BorderLayout.EAST);
-
-
-        Panel.setLayout(new BorderLayout(5,5));
-        Panel.add(Top, BorderLayout.CENTER);
-        Panel.add(Bottom, BorderLayout.SOUTH);
-
-
-        Card.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 1, Color.GRAY));
-
-        mainFrame.add(Panel);
-
-        mainFrame.revalidate();
     }
 }
